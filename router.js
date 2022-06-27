4a436e64c5e0ab1e04021a10f4c277c0346eb878
NIhilth <91199065+NIhilth@users.noreply.github.com>
2022-06-27 19:25:18 -0300

    hehehe

---
+++ router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//const users = require('./api/users/users.controller');
+
+
+//router.use("/users", users);
+
+module.exports = router;
