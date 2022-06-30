23233bd0c0a306e74d2206736100e116d42ec7bc
NIhilth <91199065+NIhilth@users.noreply.github.com>
2022-06-29 21:51:29 -0300

    nova chave

--- crud/index.js
+++ crud/index.js
@@ -12,15 +12,16 @@ const {
     deleteDoc,
   } = require("firebase/firestore/lite");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBfaUK8aVYJqds2Rmz8jlzXlxWqK_vIOJk",
-  authDomain: "biblioteca-4c49d.firebaseapp.com",
-  projectId: "biblioteca-4c49d",
-  storageBucket: "biblioteca-4c49d.appspot.com",
-  messagingSenderId: "963637999389",
-  appId: "1:963637999389:web:3f09eff56b6633df6153f4",
-  measurementId: "G-C93764S15Z"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBfaUK8aVYJqds2Rmz8jlzXlxWqK_vIOJk",
+    authDomain: "biblioteca-4c49d.firebaseapp.com",
+    databaseURL: "https://biblioteca-4c49d-default-rtdb.firebaseio.com",
+    projectId: "biblioteca-4c49d",
+    storageBucket: "biblioteca-4c49d.appspot.com",
+    messagingSenderId: "963637999389",
+    appId: "1:963637999389:web:3f09eff56b6633df6153f4",
+    measurementId: "G-C93764S15Z"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
